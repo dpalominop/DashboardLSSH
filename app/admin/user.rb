6c2a91627499314ecf05be83400e3ccd45623857
Daniel Palomino <dapalominop@gmail.com>
2017-06-21 14:53:57 -0500

    Adherido username a vista User de ActiveAdmin.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :username
 
   index do
     selectable_column
     id_column
     column :email
+    column :username
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -19,7 +20,9 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :username
       f.input :password
+#      f.input :password, :label => "New Password"
       f.input :password_confirmation
     end
     f.actions
