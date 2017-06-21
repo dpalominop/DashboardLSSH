ec4a34872255ca9e448a1ee38598fe40a2b5ec14
Daniel Palomino <dapalominop@gmail.com>
2017-06-21 14:46:51 -0500

    Cambio de ruta de ActiveAdmin a la raíz.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
+  root to: "admin/dashboard#index"
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
