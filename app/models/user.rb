061ee4e55a1a257387fe5b33d6e9c0020e497a87
Daniel Palomino <dapalominop@gmail.com>
2017-06-21 14:58:24 -0500

    Cambio de email a username para autenticación.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
 end
