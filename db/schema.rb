e87efbd8081a409dd386d4732e6142fb4b71edde
Daniel Palomino <dapalominop@gmail.com>
2017-06-23 10:10:00 -0500

    Migración y agregado modelos nuevos a ActiveAdmin.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623142630) do
+ActiveRecord::Schema.define(version: 20170623150620) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170623142630) do
   create_table "areas", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "network_elements", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "ip"
+    t.integer "port"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
