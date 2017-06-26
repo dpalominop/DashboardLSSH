6a06052b266bb28d36d2d3c45dc2de8727adf7cc
Daniel Palomino <dapalominop@gmail.com>
2017-06-26 11:11:37 -0500

    Cambio de vista Show y Form de Model Command List para que incluya la relación con Model Command.

--- app/admin/command.rb
+++ app/admin/command.rb
@@ -2,8 +2,7 @@ ActiveAdmin.register Command do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
-#
+permit_params :name
 # or
 #
 # permit_params do
--- app/admin/command_list.rb
+++ app/admin/command_list.rb
@@ -1,15 +1,48 @@
 ActiveAdmin.register CommandList do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :name, :description, :network_element_id, :role_id
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+    permit_params :name, :description, :network_element_id, :role_id, command_ids: []
+    
+    index :title => "Commands Lists" do
+        selectable_column
+        id_column
+        column :name
+        column :description
+        column :network_element_id
+        column :role_id
+        actions
+    end
+
+    filter :name
+    filter :description
+    filter :network_element_id
+    filter :role_id
+
+    form do |f|
+        f.inputs "Commands Lists Details" do
+            f.input :name
+            f.input :description
+            f.input :network_element_id, as: :select, collection: NetworkElement.all, :label => 'Network Elements'
+            f.input :role_id, as: :select, collection: Role.all, :label => 'Roles'
+            f.input :command_ids, as: :check_boxes, collection: Command.all, :label => 'Commands'
+        end
+        f.actions
+    end
+
+    show do
+        panel "Commands" do
+            table_for command_list.commands do
+                column :name
+            end
+        end
+    end
+
+    sidebar "Commands Lists Details", only: :show do
+        attributes_table_for area do
+            row :name
+            row :description
+            row :network_element_id
+            row :role_id
+        end
+    end
 
 end
