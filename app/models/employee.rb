20ad47e983e9e3b2777e4be66d43e23df8333f90
Daniel Palomino <dapalominop@gmail.com>
2017-07-05 11:10:57 -0500

    Removido obligatoriedad de campo area_id en modelo Employee.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,13 +1,9 @@
 class Employee < ApplicationRecord
-    belongs_to :area
+    #belongs_to :area
 
     has_many :command_list_employees, :dependent => :destroy
     has_many :command_lists, through: :command_list_employees
 
     validates :username,    :presence => true
     validates :document,    :presence => true
-    validates :area_id,     :presence => false
-    validates :name,        :presence => false
-    validates :lastname,    :presence => false
-    validates :command_list_ids, :presence => false
 end
