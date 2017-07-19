cdd9b6d4605e94124bab9830f515947291c885de
Daniel Palomino <dapalominop@gmail.com>
2017-07-19 15:30:48 -0500

    Ocultados campos de modelo DefaultSettings.

--- app/admin/default_permission.rb
+++ app/admin/default_permission.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register DefaultPermission do
   menu :parent => "System", :if => proc{ can? :manage, User}
-  permit_params :forbidden, :warning_counter, :intro, :prompt, :timer, :strict, :history_file
+  #permit_params :forbidden, :warning_counter, :intro, :prompt, :timer, :strict, :history_file
+  permit_params :history_file
   actions :index, :update, :edit
 
   config.filters = false
@@ -8,14 +9,22 @@ ActiveAdmin.register DefaultPermission do
   index :title => "Default Permissions", :download_links => false do
       selectable_column
       #id_column
-      column :forbidden
-      column :warning_counter
-      column :intro
-      column :prompt
-      column :timer
-      column :strict
+      #column :forbidden
+      #column :warning_counter
+      #column :intro
+      #column :prompt
+      #column :timer
+      #column :strict
       column :history_file
+      column :updated_at
       actions
+  end
+
+  form do |f|
+      f.inputs "Default Permissions" do
+          f.input :history_file
+      end
+      f.actions
   end
 
 end
