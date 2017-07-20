4a3038a63938f99d2aead12e99886314525c703a
Daniel Palomino <dapalominop@gmail.com>
2017-07-20 10:25:45 -0500

    Sincronización de controladores de creation, update y destroy de tabla Command con tabla SudoCommand.

--- app/admin/command.rb
+++ app/admin/command.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Command do
-    menu :parent => "Security Management", :priority => 1
+  menu :parent => "Security Management", :priority => 1
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,5 +11,20 @@ ActiveAdmin.register Command do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+  member_action :update, method: [:put, :patch] do
+    SudoCommand.find(params[:id]).update(name: params[:command][:name])
+    update!
+  end
+
+  member_action :create, method: [:post] do
+    SudoCommand.create(name: params[:command][:name])
+    create!
+  end
+
+  member_action :destroy, method: [:delete] do
+    SudoCommand.find(params[:id]).destroy
+    destroy!
+  end
 
 end
