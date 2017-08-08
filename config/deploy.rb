ddc1b26abd72fa0ea8115531fa6a8f8ffc50ed7d
Daniel Palomino <dapalominop@gmail.com>
2017-08-08 18:37:20 -0500

    Removida migración redundante. Set gemset.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,3 +34,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :rvm_ruby_version, 'ruby-2.4.1@DashboardLssh'
--- db/migrate/20170718150951_add_index_ip_to_network_elements.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexIpToNetworkElements < ActiveRecord::Migration[5.1]
-  def change
-    add_index :network_elements, :ip, unique: true
-  end
-end
