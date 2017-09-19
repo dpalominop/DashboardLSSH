8cbfd7c737c55a6310995ad116112fb65aadd69d
Daniel Palomino <dapalominop@gmail.com>
2017-09-19 18:24:24 -0500

    Cambio de zona horaria.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module DashboardLSSH
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :es
-
+    config.time_zone = 'Lima'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
