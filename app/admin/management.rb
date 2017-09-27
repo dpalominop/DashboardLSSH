8780eff70776d1fc358764380cc344b4db4e2b72
Daniel Palomino <dapalominop@gmail.com>
2017-09-27 15:45:46 -0500

    Importación de Gerencias.

--- app/admin/management.rb
+++ app/admin/management.rb
@@ -1,6 +1,27 @@
 ActiveAdmin.register Management do
   menu :parent => I18n.t("active_admin.employee_management"),
        :priority => 3
+       active_admin_import validate: true,
+                             template: 'import' ,
+                             template_object: ActiveAdminImport::Model.new(
+                                 hint: I18n.t("active_admin.hint_csv_import"),
+                                 force_encoding: :auto,
+                                 csv_options: { col_sep: ",", row_sep: nil, quote_char: nil }
+                             ),
+                             headers_rewrites: { 'direction' => 'direction_id'
+                                               },
+                             before_batch_import: ->(importer){
+                                 begin
+                                   direction_names = importer.values_at('direction_id')
+                                   # replacing author name with author id
+                                   directions = Direction.where(name: direction_names).pluck(:name, :id)
+                                   options   = Hash[*directions.flatten] # #{"Jane" => 2, "John" => 1}
+                                   importer.batch_replace('direction_id', options) #replacing "Jane" with 1, etc
+                                 rescue
+
+                                 end
+                             },
+                             back: -> { config.namespace.resource_for(Management).route_collection_path }
   permit_params :name, :description, :direction_id
 
   filter :name, :label => I18n.t("active_admin.name")
